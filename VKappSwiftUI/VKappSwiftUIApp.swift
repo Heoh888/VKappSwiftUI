e2267038e731e4e33b2b26f05ba0ab8b40f015f6
Aleks <heoh888@ya.ru>
2022-07-15 02:28:43 +0300

    Initial Commit

---
+++ VKappSwiftUI/VKappSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKappSwiftUIApp.swift
+//  VKappSwiftUI
+//
+//  Created by Алексей Ходаков on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKappSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
